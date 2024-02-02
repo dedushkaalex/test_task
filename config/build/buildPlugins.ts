0022b74cb45c2217c0e966767f2762170229c68d
dedushkaalex <erchenkoaleksei@yandex.ru>
2024-02-02 11:19:21 +0300

    init: start task

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({ template: paths.html }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+}
